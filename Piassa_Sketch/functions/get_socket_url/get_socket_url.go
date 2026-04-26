d6b7ec29cb2517ba05fe15350c8051d05c76ad3c
massi-bjl <massi.bjl@gmail.com>
2026-04-26 20:15:16 +0100

    ss

--- Piassa_Sketch/functions/get_socket_url/get_socket_url.go
+++ Piassa_Sketch/functions/get_socket_url/get_socket_url.go
@@ -1,50 +1,52 @@
 package lib
 
 import (
-    "crypto/md5"
-    "encoding/hex"
-    "github.com/taubyte/go-sdk/event"
-    http "github.com/taubyte/go-sdk/http/event"
-    pubsub "github.com/taubyte/go-sdk/pubsub/node"
+	"crypto/md5"
+	"encoding/hex"
+
+	"github.com/taubyte/go-sdk/event"
+	http "github.com/taubyte/go-sdk/http/event"
+	pubsub "github.com/taubyte/go-sdk/pubsub/node"
 )
 
 func fail(h http.Event, err error, code int) uint32 {
-    h.Write([]byte(err.Error()))
-    h.Return(code)
-    return 1
+	h.Write([]byte(err.Error()))
+	h.Return(code)
+	return 1
 }
 
-//export GetSketchSocket
+//export getsocketurl
 func getsocketurl(e event.Event) uint32 {
-    h, err := e.HTTP()
-    if err != nil {
-        return 1
-    }
+	h, err := e.HTTP()
+	if err != nil {
+		return 1
+	}
 
-    // Get room name from query (?room=piassa-room-1)
-    room, err := h.Query().Get("room")
-    if err != nil {
-        return fail(h, err, 500)
-    }
+	// 1. Get room from query (?room=piassa1)
+	room, err := h.Query().Get("room")
+	if err != nil {
+		return fail(h, err, 500)
+	}
 
-    // Hash the room name to ensure valid channel naming
-    hash := md5.New()
-    hash.Write([]byte(room))
-    roomHash := hex.EncodeToString(hash.Sum(nil))
+	// 2. Hash the room to create a unique internal channel name
+	hash := md5.New()
+	hash.Write([]byte(room))
+	roomHash := hex.EncodeToString(hash.Sum(nil))
 
-    // Open the channel sketch-battle-<hash>
-    channel, err := pubsub.Channel("sketch-battle-" + roomHash)
-    if err != nil {
-        return fail(h, err, 500)
-    }
+	// 3. Open the channel (Must match the regex in messaging.yml)
+	channel, err := pubsub.Channel("sketch-battle-" + roomHash)
+	if err != nil {
+		return fail(h, err, 500)
+	}
 
-    // Generate the relay URL
-    url, err := channel.WebSocket().Url()
-    if err != nil {
-        return fail(h, err, 500)
-    }
+	// 4. Get the websocket relay URL
+	url, err := channel.WebSocket().Url()
+	if err != nil {
+		return fail(h, err, 500)
+	}
 
-    // Return the path so the Next.js app can connect
-    h.Write([]byte(url.Path))
-    return 0
+	// 5. Write the path to the response
+	h.Write([]byte(url.Path))
+
+	return 0
 }
